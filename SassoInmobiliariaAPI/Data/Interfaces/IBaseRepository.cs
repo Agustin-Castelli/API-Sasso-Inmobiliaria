9b2d2d03c375882703d00075a70ad890d93a53c8
Gero <gerojbro@gmail.com>
2024-11-21 18:58:39 -0300

    Repositorios hechos 2

--- SassoInmobiliariaAPI/Data/Interfaces/IBaseRepository.cs
+++ SassoInmobiliariaAPI/Data/Interfaces/IBaseRepository.cs
@@ -1,6 +1,11 @@
 ﻿namespace SassoInmobiliariaAPI.Data.Interfaces
 {
-    public interface IBaseRepository
+    public interface IBaseRepository<T> where T : class
     {
+        List<T> GetAll();
+        T? GetById(int id);
+        T Create(T entity);
+        void Delete(T entity);
+        void Update(T entity);
     }
 }
--- SassoInmobiliariaAPI/Data/Repositories/PropertyRepository.cs
+++ SassoInmobiliariaAPI/Data/Repositories/PropertyRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace SassoInmobiliariaAPI.Data.Repositories
+﻿using SassoInmobiliariaAPI.Data.Interfaces;
+using SassoInmobiliariaAPI.Data.Services;
+using SassoInmobiliariaAPI.Models.Entities;
+
+namespace SassoInmobiliariaAPI.Data.Repositories
 {
-    public class PropertyRepository
+    public class PropertyRepository : BaseRepository<Property>, IPropertyRepository
     {
+        public PropertyRepository(ApplicationContext context) : base(context)
+        {
+
+        }
     }
 }
