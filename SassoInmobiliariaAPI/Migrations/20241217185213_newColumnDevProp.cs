b6ec86e285ef557b9c3cb693bf7de4f72eb68fb5
Agustin Castelli <rev0.c4chomax@gmail.com>
2024-12-17 15:55:44 -0300

    test database

---
+++ SassoInmobiliariaAPI/Migrations/20241217185213_newColumnDevProp.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SassoInmobiliariaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class newColumnDevProp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StateOfDevelop",
+                table: "DevelopmentProps",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StateOfDevelop",
+                table: "DevelopmentProps");
+        }
+    }
+}
