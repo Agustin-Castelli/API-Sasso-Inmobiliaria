4ae734a813e9403736dd584f5e0fbe691b07697c
facundolgomez <facundolgomez87@gmail.com>
2024-12-18 15:24:26 -0300

    migrations

--- SassoInmobiliariaAPI/Migrations/20241217202736_newColumnStateOfDevelop.Designer.cs
+++ SassoInmobiliariaAPI/Migrations/20241217202736_newColumnStateOfDevelop.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SassoInmobiliariaAPI.Data.Services;
 
@@ -11,9 +12,11 @@ using SassoInmobiliariaAPI.Data.Services;
 namespace SassoInmobiliariaAPI.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20241217202736_newColumnStateOfDevelop")]
+    partial class newColumnStateOfDevelop
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SassoInmobiliariaAPI/Migrations/20241217202736_newColumnStateOfDevelop.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SassoInmobiliariaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class newColumnStateOfDevelop : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StateOfDevelop",
+                table: "Propertys",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StateOfDevelop",
+                table: "Propertys");
+        }
+    }
+}
